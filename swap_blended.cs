8e485fae3b772849e73993fa967d0c04f811bd63
Kamran-Sohail <106171774+Kamran-Sohail@users.noreply.github.com>
2024-02-13 22:02:45 +0500

    Add files via upload

---
+++ swap_blended.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class program
+{
+
+    public static void Main(string[] args)
+    {
+
+        int num1, num2, temp;
+
+
+        Console.WriteLine("Enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+
+        Console.Write("\nAfter Swapping : ");
+        Console.Write("\nFirst Number : " + num1); // Displaying the first number after swapping
+        Console.Write("\nSecond Number : " + num2); // Displaying the second number after swapping
+
+        Console.Read(); // Keeping the console window open until a key is pressed
+    }
+}
