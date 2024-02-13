1a43a8a050f3de1b47ceb35d475e2fa57209322c
Kamran-Sohail <106171774+Kamran-Sohail@users.noreply.github.com>
2024-02-13 21:47:27 +0500

    Add files via upload

---
+++ swap_ChatGPT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1, num2, temp;
+
+        Console.WriteLine("Enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Before swapping: num1 = {num1}, num2 = {num2}");
+
+        // Swapping logic using a temporary variable
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        Console.WriteLine($"After swapping: num1 = {num1}, num2 = {num2}");
+        Console.Read();
+    }
+}
